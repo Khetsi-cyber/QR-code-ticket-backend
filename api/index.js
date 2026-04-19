131abf1636183f9ea659e1bac9bb940d25c77848
Khetsi-cyber <tiyandzamshengu@gmail.com>
2026-04-19 01:33:29 +0000

    Fix 405 payment API routing

--- api/index.js
+++ api/index.js
@@ -1,8 +1,3 @@
-import serverless from 'serverless-http';
 import app from '../payment-server.js';
 
-const handler = serverless(app);
-
-export default async function vercelHandler(req, res) {
-  return handler(req, res);
-}
+export default app;
